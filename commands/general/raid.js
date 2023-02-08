8ed8dc13636f06a0ab4d4c4f5fd40f482c687414
Mathis Boulais <mathis.oxym@gmail.com>
2023-02-08 15:56:52 +0100

    windows

--- commands/general/raid.js
+++ commands/general/raid.js
@@ -1,34 +1,53 @@
-// const { ActionRowBuilder } = require('@discordjs/builders');
-// const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, PermissionFlagsBits, ButtonStyle } = require('discord.js');
+const { ActionRowBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, PermissionFlagsBits, ButtonStyle } = require('discord.js');
 
-// module.exports = {
-//     data: new SlashCommandBuilder()
-//         .setName('raid')
-//         .setDescription('propose un raid au membre avec timer'),
-//     async execute(interaction) {
-//         const {options,member,guild} = interaction;
-//         const raid = options.getString('raid');
-//         const timer = options.getString('timer');
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('raid')
+        .setDescription('Raid token gloot')
+        .setDefaultPermission(false)
+        //time before delete message
+        .addIntegerOption(option => option.setName('time').setDescription('Time before delete message').setRequired(true))
+        //jeu sur lequel on fait le raid
+        .addStringOption(option => option.setName('game').setDescription('Game').setRequired(true))
+            //choices
+            .addChoice('Valorant', 'Valorant')
+            .addChoice('League of Legends', 'League of Legends')
+            .addChoice('Apex Legends', 'Apex Legends')
+            .addChoice('Rocket League', 'Rocket League')
+            .addChoice('Overwatch', 'Overwatch')
+            .addChoice('CS:GO', 'CS:GO')
+            .addChoice('PUBG', 'PUBG')
+            .addChoice('Dota 2', 'Dota 2')
 
-//         const embed = new EmbedBuilder()
-//             .setColor('Orange')
-//             .setAuthor({ name: member.user.tag, iconURL: member.user.displayAvatarURL({ dynamic: true }) })
-//             .addFields(
-//                 { name: "Raid:", value: raid, inline: false },
-//                 { name: "Timer:", value: timer, inline: true },
-//             )
-//             .setTimestamp()
+        .addIntegerOption(option => option.setName('token').setDescription('Token').setRequired(true)),
+    async execute(interaction) {
+        //On récupère les options
+        const time = interaction.options.getInteger('time');
+        const game = interaction.options.getString('game');
+        const token = interaction.options.getInteger('token');
+        //On crée le message
+        const embed = new EmbedBuilder()
+            .setTitle(`Raid ${game}`)
+            .setDescription(`Token : ${token}`)
+            .setColor('#ff0000')
+            .setTimestamp();
+        //On crée le bouton
+        const button = new ButtonBuilder()
+            .setCustomId('raid')
+            .setLabel('Gloot')
+            .setStyle(ButtonStyle.PRIMARY);
+        //On crée la row
+        const row = new ActionRowBuilder()
+            .addComponents(button);
+        //On envoie le message
+        const message = await interaction.reply({ embeds: [embed], components: [row], fetchReply: true });
+        //On supprime le message après le temps
+        setTimeout(() => {
+            message.delete();
+        }, time * 1000);
+    }
+};
 
-//         const button = new ActionRowBuilder().addComponents(
-//             new ButtonBuilder().setCustomId('participe').setLabel('Je participe').setStyle(ButtonStyle.Success),
-//             new ButtonBuilder().setCustomId('participe-pas').setLabel('Je ne participe pas').setStyle(ButtonStyle.Danger),
-//         );
 
-//         try {
-//             const message = await interaction.reply({ embeds: [embed], components: [button], fetchReply: true });
-//         }
-//         catch (error) {
-//             console.log(error);
-//         }
-//     }
-// }
+
